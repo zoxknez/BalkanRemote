45634dd457ddb2a6c8eb42caa9f5773059a4b171
zoxknez <zoxknez@hotmail.com>
2025-10-05 04:19:47 +0200

    chore: obrisani nekorišćeni fajlovi i duplikati (FirmeContent, hooks, SQL fix fajlovi, migrations backup)

--- supabase/migrations_backup/20250928_jobs_public_view.sql
+++
@@ -1,9 +0,0 @@
--- Public view over jobs without raw/internal columns
-create or replace view jobs_public_v as
-  select stable_key, source_id, source_name, title, company, location, remote,
-         salary_min, salary_max, salary_currency, posted_at, apply_url
-  from jobs;
-
--- Principle of least privilege: revoke broad anon access to table and grant to view
-revoke all on table jobs from anon;
-grant select on jobs_public_v to anon;
